d7294412efa1557f14e63dec577039b1e4718bc3
James Budday <jsoftwareengineering@users.noreply.github.com>
2017-07-04 18:56:40 +0200

    make all employee boxes on a row the same height

--- web/js/equalHeightColumns.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    var heights = $('.col-lg-4.col-md-6.text-center').map(function() {
-        return $(this).height();
-    }).get(),
-    maxHeight = Math.max.apply(null, heights);
-
-    $('.col-lg-4.col-md-6.text-center').height(maxHeight);
-});
